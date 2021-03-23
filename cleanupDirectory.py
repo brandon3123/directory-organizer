fcf68cd523475e810a3314ef71909d99d391d098
Brandon Nolan <brandonnolan3123@gmail.com>
2021-03-23 15:06:34 -0600

    renaming

--- cleanupDirectory.py
+++ cleanupDirectory.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     arguments = sys.argv
 
     if len(arguments) == 2:
-        directory_to_cleanup = arguments[1]
-        cleanup_service.clean_up_directory(directory_to_cleanup)
+        directory = arguments[1]
+        cleanup_service.clean_up_directory(directory)
     else:
         print("Please supply the full directory path of which to be organized. Only 1 directory can be specified.")
         sys.exit(0)
